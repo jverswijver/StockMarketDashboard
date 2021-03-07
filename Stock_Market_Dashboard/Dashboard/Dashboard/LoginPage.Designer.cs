d76d24ef9042953238b175cbeadc930fd6454fc9
Lapud <Lapud@users.noreply.github.com>
2021-03-07 01:02:30 -0600

    LoginPage

---
+++ Stock_Market_Dashboard/Dashboard/Dashboard/LoginPage.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace Dashboard
+{
+    partial class LoginPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginPage));
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnSignin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSignup = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.smdLogo = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.smdLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.AcceptsReturn = true;
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUsername.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtUsername.Font = new System.Drawing.Font("Arial", 12F);
+            this.txtUsername.ForeColor = System.Drawing.Color.DimGray;
+            this.txtUsername.Location = new System.Drawing.Point(75, 185);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(0);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(203, 26);
+            this.txtUsername.TabIndex = 8;
+            this.txtUsername.TabStop = false;
+            this.txtUsername.Text = "Username";
+            this.txtUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtUsername.WordWrap = false;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.Font = new System.Drawing.Font("Arial", 12F);
+            this.txtPassword.ForeColor = System.Drawing.Color.DimGray;
+            this.txtPassword.Location = new System.Drawing.Point(75, 228);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(203, 26);
+            this.txtPassword.TabIndex = 8;
+            this.txtPassword.Text = "Password";
+            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtPassword.WordWrap = false;
+            // 
+            // btnSignin
+            // 
+            this.btnSignin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(159)))), ((int)(((byte)(253)))));
+            this.btnSignin.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnSignin.FlatAppearance.BorderSize = 0;
+            this.btnSignin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btnSignin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnSignin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSignin.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnSignin.ForeColor = System.Drawing.Color.White;
+            this.btnSignin.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnSignin.Location = new System.Drawing.Point(111, 278);
+            this.btnSignin.Name = "btnSignin";
+            this.btnSignin.Size = new System.Drawing.Size(135, 26);
+            this.btnSignin.TabIndex = 9;
+            this.btnSignin.Text = "Log in";
+            this.btnSignin.UseVisualStyleBackColor = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Transparent;
+            this.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.btnExit.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnExit.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnExit.Location = new System.Drawing.Point(603, 0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(26, 26);
+            this.btnExit.TabIndex = 18;
+            this.btnExit.Text = "X";
+            this.btnExit.UseVisualStyleBackColor = false;
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold);
+            this.lblWelcome.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lblWelcome.Location = new System.Drawing.Point(51, 133);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(253, 32);
+            this.lblWelcome.TabIndex = 14;
+            this.lblWelcome.Text = "WELCOME to SMD ";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.btnSignup);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblWelcome);
+            this.panel1.Controls.Add(this.smdLogo);
+            this.panel1.Controls.Add(this.txtUsername);
+            this.panel1.Controls.Add(this.txtPassword);
+            this.panel1.Controls.Add(this.btnSignin);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.panel1.ForeColor = System.Drawing.Color.Transparent;
+            this.panel1.Location = new System.Drawing.Point(154, 36);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(341, 361);
+            this.panel1.TabIndex = 19;
+            // 
+            // btnSignup
+            // 
+            this.btnSignup.BackColor = System.Drawing.Color.Transparent;
+            this.btnSignup.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnSignup.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnSignup.FlatAppearance.BorderSize = 0;
+            this.btnSignup.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnSignup.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnSignup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSignup.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnSignup.ForeColor = System.Drawing.Color.White;
+            this.btnSignup.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnSignup.Location = new System.Drawing.Point(203, 323);
+            this.btnSignup.Name = "btnSignup";
+            this.btnSignup.Size = new System.Drawing.Size(91, 26);
+            this.btnSignup.TabIndex = 16;
+            this.btnSignup.Text = "Sign up";
+            this.btnSignup.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnSignup.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F);
+            this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label1.Location = new System.Drawing.Point(40, 327);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 18);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Don\'t have an account?";
+            // 
+            // smdLogo
+            // 
+            this.smdLogo.BackColor = System.Drawing.Color.Transparent;
+            this.smdLogo.Image = ((System.Drawing.Image)(resources.GetObject("smdLogo.Image")));
+            this.smdLogo.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.smdLogo.Location = new System.Drawing.Point(111, 17);
+            this.smdLogo.Name = "smdLogo";
+            this.smdLogo.Size = new System.Drawing.Size(135, 113);
+            this.smdLogo.TabIndex = 12;
+            this.smdLogo.TabStop = false;
+            // 
+            // LoginPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(633, 442);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.panel1);
+            this.Name = "LoginPage";
+            this.Opacity = 0.98D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoginPage";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.smdLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnSignin;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSignup;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox smdLogo;
+    }
+}
---
+++ Stock_Market_Dashboard/Dashboard/Dashboard/LoginPage.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Dashboard
+{
+    public partial class LoginPage : Form
+    {
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+(
+    int nLeftRect,     // x-coordinate of upper-left corner
+    int nTopRect,      // y-coordinate of upper-left corner
+    int nRightRect,    // x-coordinate of lower-right corner
+    int nBottomRect,   // y-coordinate of lower-right corner
+    int nWidthEllipse, // width of ellipse
+    int nHeightEllipse // height of ellipse
+);
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginPage_Load(object sender, EventArgs e)
+        {
+            txtUsername.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, txtUsername.Width, txtUsername.Height, 15, 15));
+            txtPassword.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, txtPassword.Width, txtPassword.Height, 15, 15));
+            btnSignin.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, btnSignin.Width, btnSignin.Height, 15, 15));
+            btnSignup.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, btnSignup.Width, btnSignup.Height, 15, 15));
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSignin_MouseClick(object sender, MouseEventArgs e)
+        {
+            btnSignin.BackColor = System.Drawing.Color.MediumSeaGreen;
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            txtPassword.UseSystemPasswordChar = true;
+        }
+
+        private void txtPassword_Click(object sender, EventArgs e)
+        {
+            txtPassword.Text = "";
+        }
+
+        private void txtUsername_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtUsername.Text = "";
+        }
+    }
+}
